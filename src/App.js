63e45af9e660db45d0e8afbf71a76104584b3857
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-13 16:46:55 -0500

    wrap all components in GlobalProvider

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
 
+import { GlobalProvider } from "./context/GlobalState";
+
 function App() {
 	return (
-		<div className="App">
+		<GlobalProvider>
 			<Header />
 			<div className="container"></div>
 			<Balance />
 			<IncomeExpenses />
 			<TransactionList />
 			<AddTransaction />
-		</div>
+		</GlobalProvider>
 	);
 }
 
