e989b89c69861962f360861d92ef13d351b8c8b1
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-14 16:25:31 -0500

    fix bugs

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -12,7 +12,9 @@ export const AddTransaction = () => {
 		const newTransaction = {
 			id: Math.floor(Math.random() * 100000000),
 			text,
+			amount: +amount,
 		};
+		addTransaction(newTransaction);
 	};
 
 	return (
